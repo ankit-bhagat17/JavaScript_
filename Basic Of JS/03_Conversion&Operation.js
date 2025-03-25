74b6f7f2f70b75a3e789de02cd10dda24f148387
Ankit Bhagat <130461518+ankit-bhagat17@users.noreply.github.com>
2025-03-25 13:54:09 +0000

    conversion of datatypes

--- Basic Of JS/03_Conversion&Operation.js
+++ Basic Of JS/03_Conversion&Operation.js
@@ -1,8 +1,26 @@
 let score = null;
-console.log( score);
+// console.log( score);
 
 
-let valueInNumber = Number(score);
-console.log(typeof (valueInNumber));
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
 
 
+// "33" => 33
+// "33abc" => Nan
+// true =>; false => 0;
+
+let isLoggedIn = "Ankit";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false;
+
+//"" => false;
+// "Ankit" => true;
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
