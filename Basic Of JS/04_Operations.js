2f57c0f2cf6c18349b2307174aec40091b362c69
Ankit Bhagat <130461518+ankit-bhagat17@users.noreply.github.com>
2025-03-25 14:20:39 +0000

    Operations

---
+++ Basic Of JS/04_Operations.js
@@ -0,0 +1,34 @@
+// ************************* Operations *************************
+
+let value = 3;
+let  negValue = - value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1 = "Hello";
+let str2 = "Ankit";
+// console.log(str1 +" "+str2);
+
+// console.log(2 >  1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 !=  1);
+// console.log(2 >= 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0 );
+// console.log(null == 0 );
+// console.log(null >= 0 );
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+console.log("2" === 2);
