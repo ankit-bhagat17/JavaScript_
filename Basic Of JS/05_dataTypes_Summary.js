7d864041d12ee768ff0811c6a781815910860184
Ankit Bhagat <130461518+ankit-bhagat17@users.noreply.github.com>
2025-03-25 14:58:01 +0000

    Datatypes Summary

---
+++ Basic Of JS/05_dataTypes_Summary.js
@@ -0,0 +1,33 @@
+//  Primitive Data Types
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false; 
+const outSideTemp = null;
+let userEmail;
+
+// const id = Symbole('123');
+// const anotherId = Symbol('123');
+// console.log(id === anotherId);//false
+
+const bigNumber = 644445555564646449494n;
+
+// Refference (Non Primitive)
+
+//Array , Objects, Functions;
+
+const heros = ["Shaktiman","nnagraj","doga"];
+let myObj={
+    name:"Ankit",
+    age : 21
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber);
